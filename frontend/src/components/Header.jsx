2847d1f41680c4c674c6396e48027b33f44056e7
Sathish-30 <sathish.amsaravanan@gmail.com>
2024-01-08 10:16:39 +0530

    Added Header content for the application

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,14 @@
 const Header = () => {
     return (
         <>
-            <header className="flex items-center lg:pt-[20px] md:pt-[15px] sm:pt-[12px] sm:justify-center md:justify-between">
-                <div className="ml-[100px]">
+            <header className="bg-white shadow flex items-center lg:py-[20px] md:py-[15px] sm:py-[12px] vsm:py-2 sm:justify-center vsm:justify-center md:justify-between md:text-lg vsm:text-sm sm:text-base dark:bg-black dark:text-slate-50">
+                <div className="md:pl-[100px] cursor-pointer">
                     <h4 className="font-semibold">Coding - G</h4>
                 </div>
-                <ol className="mr-[100px] sm:hidden md:flex ">
-                    <li className="mx-[2rem]">Home</li>
-                    <li className="mx-[2rem]">About</li>
-                    <li className="mx-[2rem]">Contact</li>
+                <ol className="md:pr-[100px] vsm:hidden sm:hidden md:flex md:text-lg vsm:text-sm sm:text-base">
+                    <li className="px-[2rem]"><a className="cursor-pointer hover:text-black hover:font-semibold dark:hover:text-sky-200">Home</a></li>
+                    <li className="px-[2rem]"><a className="cursor-pointer hover:text-black hover:font-semibold dark:hover:text-sky-200">About</a></li>
+                    <li className="pl-[2rem]"><a className="cursor-pointer hover:text-black hover:font-semibold dark:hover:text-sky-200">Contact</a></li>
                 </ol>
             </header>
         </>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,18 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+      screens: {
+          'vsm': {'min': '0px', 'max': '575px'},
+          // => @media (min-width: 576px) { ... }
+          'sm': '576px',
+          // => @media (min-width: 768px) { ... }
+          'md': '768px',
+          // => @media (min-width: 992px) { ... }
+          'lg': '992px',
+          // => @media (min-width: 1200px) { ... }
+          'xl': '1200px',
+
+      },
     extend: {},
   },
   plugins: [],
